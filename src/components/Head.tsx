1730ef67144c466b4cc0ddb46a01445e21836afd
syofyanzuhad <sofyanzuhad2@gmail.com>
2023-01-16 19:10:09 +0700

    update: metaTag-headComponent

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -19,7 +19,47 @@ const Avatar = ({
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta name="author" content={author} />
+
+      <meta property="og:title" content={title} />
       <meta property="og:image" content={image} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:type" content="website" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@syofyanzuhad" />
+      <meta name="twitter:creator" content="@syofyanzuhad" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+
+      <meta name="theme-color" content="#000000" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+      <meta name="application-name" content="FreeBio" />
+      <meta name="apple-mobile-web-app-title" content="FreeBio" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <link rel="icon" href="/favicon.ico" />
       <script async src="https://cdn.splitbee.io/sb.js"></script>
     </Head>
