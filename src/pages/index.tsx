a06aba81fb51cd34a787b82e9a70030868cfbae4
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2023-08-20 18:15:47 +0700

    fix: index-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,26 @@ const links = userData.links;
 // console.log(userData);
 
 const Home: NextPage = () => {
+
+  const container = "md:container md:mx-auto lg:max-w-4xl px-4";
+  const mainClass = `bg-cover bg-center bg-no-repeat bg-fixed bg-[url('/neon.jpeg')] h-full min-h-screen text-[#CEEDFF]`;
+  // let styleText = theme.styleText ?? "indigo";
+  // switch (theme.style) {
+	// 	case "shadow":
+	// 		styleText = "shadow-link-s";
+	// 		break;
+	// 	case "gradient":
+	// 		styleText = "bg-gradient-to-r from-green-400 to-blue-500";
+	// 		break;
+	// 	default:
+	// 		styleText = `bg-${theme.styleColor}-500`;
+	// 		break;
+	// }
+  const navClass = `bg-white bg-opacity-10 backdrop-blur-md backdrop-filter w-full rounded-full sticky top-10 z-10 transition-all duration-300 ease-in-out`;
+	const textClass = `my-4 p-4 flex justify-center cursor-pointer items-center shadow-link-s backdrop-blur-3xl w-full min-h-12 opacity-80 rounded-full md:hover:-translate-y-0 md:hover:scale-110 duration-700 ease-in-out`;
+
   return (
-    <div className="bg-cover bg-center bg-no-repeat bg-fixed bg-[url('/neon.jpeg')] h-full min-h-screen text-[#CEEDFF]">
+    <div className={mainClass}>
       <Head
         title={`${name} | Bio Link`}
         description={`${description} | Free Bio Link`}
@@ -31,7 +49,7 @@ const Home: NextPage = () => {
 
       <main className={container}>
         <section className="flex flex-col items-center pt-16">
-          <Navbar name={name} />
+          <Navbar name={name} username={name} backgroundColor="white" textColor="#CEEDFF" rounded="full" navClass={navClass} textClass={textClass} />
           <div className="mt-3">
             <Avatar url={avatar_url} alt="avatar" />
           </div>
@@ -58,7 +76,7 @@ const Home: NextPage = () => {
         </section>
         <section className="flex flex-col items-center my-5">
           {links?.map(({ id, title, href }) => (
-            <TextLink key={id} title={title} href={href} />
+            <TextLink key={id} title={title} href={href} textClass={textClass} />
           ))}
         </section>
       </main>
