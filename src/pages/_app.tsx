66f341fa35be89f4a4a50774ed93ccc88bc548fd
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2024-03-30 19:59:29 +0700

    Remove @vercel/speed-insights package and its usage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
+      {/* <SpeedInsights /> */}
     </>
   )
 }
